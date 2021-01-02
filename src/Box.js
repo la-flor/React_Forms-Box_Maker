989629f16506a2fe1bce0a5bf19df2e0d69094ef
User <leifaflor@gmail.com>
2021-01-02 10:54:25 -0700

    init

---
+++ src/Box.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Box = () => {
+    
+}
+
+
+
+export default Box;
---
+++ src/BoxList.js
@@ -0,0 +1,10 @@
+import NewBoxForm from './NewBoxForm';
+import React, { useState } from 'react';
+
+const BoxList = () => {
+
+}
+
+
+
+export default BoxList;
